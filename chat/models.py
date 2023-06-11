ac713d6524d369c18ef37bdff541a3e2ab3bcef4
Jabez-abija2399 <jabeznew2399@gmail.com>
2023-06-11 04:12:07 -0700

    models file added

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 from datetime import datetime
-from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 login_manager = LoginManager()  # Instantiate the LoginManager
@@ -10,15 +9,12 @@ login_manager = LoginManager()  # Instantiate the LoginManager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-db = SQLAlchemy()
-
-
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     fullName = db.Column(db.String(255), nullable=False)
-    username = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
     phoneNumber = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
@@ -28,20 +24,30 @@ class User(db.Model, UserMixin):
     updated_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(),
                            onupdate=db.func.current_timestamp(), nullable=False)
 
-  
+    # Relationship with messages sent by the user
+    # Relationship with messages received by the user
+    messages_sent = db.relationship('Message', backref='sender', lazy=True, foreign_keys='Message.sender_id')
+    messages_received = db.relationship('Message', backref='receiver', lazy=True, foreign_keys='Message.receiver_id')
     def __repr__(self):
         return f"<User(id={self.id}, fullName='{self.fullName}', username='{self.username}', " \
                f"phoneNumber='{self.phoneNumber}', gender='{self.gender}')>"
 
 
-# Message model
+
+
+
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender = db.Column(db.String(50), nullable=False)
-    receiver = db.Column(db.String(50), nullable=False)
-    message = db.Column(db.Text, nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def __init__(self, sender, receiver, message):
-        self.sender = sender
-        self.receiver = receiver
-        self.message = message
+    
+    def __init__(self, sender_id, receiver_id, content, sent_time=None):
+        self.sender_id = sender_id
+        self.receiver_id = receiver_id
+        self.content = content
+        self.sent_time = sent_time or datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
